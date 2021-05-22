653d2c1edbd561f5f90f982124be4f0677708387
Kachbal <ilhamkachbal@gmail.com>
2021-05-22 02:34:20 +0200

    code first

---
+++ test_base_donnee_indemnite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="test_base_donnee_indemnite.MvcApplication" Language="C#" %>
---
+++ test_base_donnee_indemnite/Migrations/202105220016069_InitialCreate61.cs
@@ -0,0 +1,24 @@
+﻿namespace test_base_donnee_indemnite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate61 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.OrdreMissions", "grade", c => c.String());
+            AddColumn("dbo.OrdreMissions", "objetDepart", c => c.String());
+            AddColumn("dbo.OrdreMissions", "moyenTransport", c => c.String());
+            AddColumn("dbo.OrdreMissions", "nombreCheuvaux", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.OrdreMissions", "nombreCheuvaux");
+            DropColumn("dbo.OrdreMissions", "moyenTransport");
+            DropColumn("dbo.OrdreMissions", "objetDepart");
+            DropColumn("dbo.OrdreMissions", "grade");
+        }
+    }
+}
